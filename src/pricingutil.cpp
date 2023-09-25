4c8b636a9fa0fd8fb0e2835e8d1abf52d4b1f351
Jason <jasonlu2025@u.northwestern.edu>
2023-09-25 09:46:20 -0500

    Fetched ethan's most recent version

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,17 @@
 #include "pricingutil.h"
+
+//
+// Created by Ethan on 8/18/2023.
+//
+
+PricingUtil::PricingUtil() {}
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant)
+{
+    this->val = prevPrice * (0.9 + interest) * oleoConstant;
+}
+
+float PricingUtil::getVal()
+{
+    return this->val;
+}
--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,39 @@
 #include <gtest/gtest.h>
+#include "pricingutil.h"
 
-TEST(sampleTest, sample) {
+TEST(PriceUtilTests, sample)
+{
     EXPECT_EQ(4, 4);
+}
+
+TEST(PriceUtilTests, initialCalculation)
+{
+    PricingUtil pricingUtil;
+    pricingUtil.calcVal(2.0, 0.3, 4.0);
+    float res = pricingUtil.getVal();
+    EXPECT_NEAR(res, 9.6, 1e-4);
+}
+
+TEST(PriceUtilTests, negativesInCalculation)
+{
+    PricingUtil pricingUtil;
+    pricingUtil.calcVal(-2.0, 0.3, 4.0);
+    float res = pricingUtil.getVal();
+    EXPECT_NEAR(res, -9.6, 1e-4);
+}
+
+TEST(PriceUtilTests, zerosInCalculation)
+{
+    PricingUtil pricingUtil;
+    pricingUtil.calcVal(0.0, 0.3, 4.0);
+    float res = pricingUtil.getVal();
+    EXPECT_NEAR(res, 0, 1e-4);
+}
+
+TEST(PriceUtilTests, largeInterest)
+{
+    PricingUtil pricingUtil;
+    pricingUtil.calcVal(2.0, 9999, 4.0);
+    float res = pricingUtil.getVal();
+    EXPECT_NEAR(res, 79999.2, 1e-2);
 }
