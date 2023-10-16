ea287c079805ab02fafebe84570ab35e01e7c4ba
Jason <jasonlu2025@u.northwestern.edu>
2023-10-15 22:58:02 -0500

    Improved disaster

--- prime.py
+++ prime.py
@@ -1,22 +1,45 @@
 import timeit
+import math
+
+
+def calc_primes(n):
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+    prime_list = [i for i, is_prime in enumerate(primes) if is_prime]
+    return prime_list
+
 
 def disasterCode():
-    for i in range (2,2500):
-        uniquePrimes = []
-        currentPrime = i
-        for j in range (2,i):
-            checkPrime = j
-            flag = False
-            for k in range (2,checkPrime-1):
-                if (j%k==0):
-                    flag = True
-                    break
-            if not flag and i%checkPrime==0 and checkPrime <= i:
-                while (currentPrime%checkPrime==0):
-                    currentPrime/=checkPrime
-                uniquePrimes.append(checkPrime)
-        if len(uniquePrimes) == 0:
+    max_value = 2500
+    max_prime_check = int(math.sqrt(max_value))
+    primes = calc_primes(max_prime_check)
+    prime_set = set(primes)
+
+    uniquePrimes = []
+
+    for i in range(2, max_value):
+        if i in prime_set:
             uniquePrimes.append(i)
+            continue
+
+        currentPrime = i
+        for prime in primes:
+            if prime > max_prime_check or prime > i:
+                break
+            if currentPrime % prime == 0:
+                uniquePrimes.append(prime)
+                while currentPrime % prime == 0:
+                    currentPrime //= prime
+
+        if currentPrime > 1:
+            uniquePrimes.append(currentPrime)
+
 
 # Benchmark the code
 if __name__ == "__main__":
@@ -25,7 +48,7 @@ if __name__ == "__main__":
 
     # Measure the execution time of disasterCode function
     times = []
-    for i in range(0,5):
+    for i in range(0, 5):
         times.append(timeit.timeit(benchmark_code, setup=setup_code, number=1))
 
     res = sum(times)/5
